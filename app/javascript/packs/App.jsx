84ce4edcb6ab31a4b8c7ac3b6edf6779fa8d5a46
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-05-05 18:02:16 +0200

    add import jquery popper and bootstrap js

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -4,8 +4,12 @@
 
 import React from "react";
 import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import SharePropsRoutes from "../components/SharePropsRoutes";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
