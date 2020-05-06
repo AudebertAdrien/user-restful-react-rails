94d152136a1bc7e784e6237f1abac9f55b72d83e
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-05-06 14:59:50 +0200

    comment import boot etc

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -6,10 +6,10 @@ import React from "react";
 import { render } from "react-dom";
 import SharePropsRoutes from "../components/SharePropsRoutes";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+/* import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; */
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
