a91ed6dd904a187149411cc3397207a7ab0abcd4
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-05-06 15:30:42 +0200

    test07

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -9,7 +9,7 @@ import SharePropsRoutes from "../components/SharePropsRoutes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+//import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
