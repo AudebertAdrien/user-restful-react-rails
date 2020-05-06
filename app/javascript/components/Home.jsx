63c507c2f89b6064ae5968b0fbf160eca3c111a4
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-05-06 15:57:51 +0200

    text green

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,7 +5,7 @@ export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4 text-danger">Food Recipes11</h1>
+        <h1 className="display-4 text-success">Food Recipes11</h1>
         <p className="lead">
           A curated list of recipes for the best homemade meal and delicacies.
         </p>
@@ -13,7 +13,7 @@ export default () => (
         <Link to="/" className="btn btn-lg custom-button" role="button">
           View Recipes
         </Link>
-        
+
         <div class="dropdown">
           <button
             class="btn btn-secondary dropdown-toggle"
